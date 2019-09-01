e609d9fe9df0f5935bf0667bcca89a2c9b56ea59
itamard7 <itamar.demri@gmail.com>
2019-09-01 18:14:54 +0300

    Init repository

---
+++ index.js
@@ -0,0 +1,27 @@
+const puppeteer = require('puppeteer');
+const _ = require('lodash');
+const Promise = require('bluebird'); 
+const log = console;
+const sizes = require('./sizes.json');
+const url = 'localhost:3000';
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function run() {
+	 const browser = await puppeteer.launch({ headless: false });
+	 await Promise.all(_.map(sizes, async (size) => {
+		const page = await browser.newPage();
+		await page.setViewport(size);
+    await page.goto(url);
+		await page.screenshot({ path:`./shots/${size.width}X${size.height}.png` });
+		await sleep(300);
+		await page.close();
+	}));
+	await browser.close();
+}
+
+run()
+	.then(() => log.info('Completed Successfully'))
+	.catch(ex => log.error('Completed with Errors ', ex));
