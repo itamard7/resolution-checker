b89f0e726019b9a880821a8cded1aa1ffb703e79
Danny Snitzer <dannys@powerplugltd.com>
2019-09-02 14:35:35 +0300

    Added folder creation logic

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const puppeteer = require('puppeteer');
 const _ = require('lodash');
+const fs = require('fs');
 const Promise = require('bluebird'); 
 const log = console;
 const { sizes, url } = require('./config.json');
+const dir = './shots';
 
 async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function run() {
+	 if (!fs.existsSync(dir)){
+    		fs.mkdirSync(dir);
+         }
 	 const browser = await puppeteer.launch({ headless: false });
 	 await Promise.all(_.map(sizes, async (size) => {
 		const page = await browser.newPage();
 		await page.setViewport(size);
     await page.goto(url);
-		await page.screenshot({ path:`./shots/${size.width}X${size.height}.png` });
+		await page.screenshot({ path:`${dir}/${size.width}X${size.height}.png` });
 		await sleep(300);
 		await page.close();
 	}));
