67bcacfc1ec61eac693bde1937291d563cd7e08f
Danny Snitzer <dannys@powerplugltd.com>
2019-09-19 12:25:24 +0300

    Added wait for selector support

--- index.js
+++ index.js
@@ -9,20 +9,26 @@ const dir = './shots';
 async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
-
+async function handleResolution(size, browser) {
+	const page = await browser.newPage();
+	await page.setViewport(size);
+ 	await page.goto(url);
+	// Here you set the selector to wait for the loading 
+	await waitForSelector(page, 'div.table-status');
+	await page.screenshot({ path:`${dir}/${size.width}X${size.height}.png` });
+	await sleep(300);
+	await page.close();
+}
+async function waitForSelector(page, selector) {
+	return page.waitForSelector(selector, { timeout: 100000 });
+}
 async function run() {
-	 if (!fs.existsSync(dir)){
+	if (!fs.existsSync(dir)){
     		fs.mkdirSync(dir);
-         }
-	 const browser = await puppeteer.launch({ headless: false });
-	 await Promise.all(_.map(sizes, async (size) => {
-		const page = await browser.newPage();
-		await page.setViewport(size);
-    await page.goto(url);
-		await page.screenshot({ path:`${dir}/${size.width}X${size.height}.png` });
-		await sleep(300);
-		await page.close();
-	}));
+        }
+
+	const browser = await puppeteer.launch({ headless: false });
+	await Promise.all(_.map(sizes, size => handleResolution(size, browser)));
 	await browser.close();
 }
 
